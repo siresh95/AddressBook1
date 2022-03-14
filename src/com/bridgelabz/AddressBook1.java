f5ee58804cd47ede3ba255c9b850c51fb4b98285
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-14 12:22:26 +0530

    Welcome to Address Book

---
+++ src/com/bridgelabz/AddressBook1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+//import java.util.Scanner;
+
+public class AddressBook1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+	// write your code here
+
+    }
+}
